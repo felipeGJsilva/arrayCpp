cfb19eecd3170d35ebb3ae1c94a59d765af8ffea
FelipeJansen <132519887+felipeGJsilva@users.noreply.github.com>
2024-04-25 23:06:57 +0000

    f

--- revfoor.cpp
+++ revfoor.cpp
@@ -6,10 +6,14 @@ int main(){
     int a = 0;
     while (a < 10)
     {
-        cout << "Loser" << endl;
+        cout << "Loser\n" << endl;
         a = a + 1;
     }
-    
+    int b = 0;
 
+    for (size_t i = 0; i < 10;i++){
+        
+        cout << "palavra\n" << endl;
+    }
     return 0;
 }
